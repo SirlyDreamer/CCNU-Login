1968808f38e00206233ad0022ae3f8b55bc43a6d
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-10-30 02:46:52 +0000

    cc

--- login.js
+++ login.js
@@ -24,19 +24,19 @@ async function main() {
         try {
             await page.goto(uri, {waitUntil: "networkidle2"});
             await page.click('#su');
-            console.log("[INFO] 网络连接正常");
+            console.log(new Date().toLocaleString() + " | [INFO] 网络连接正常");
             await page.close();
             await browser.close();
             timer = setTimeout(daemon, 600000);
             return;
         } catch (e) {
-            console.log("[ERR] 网络连接断开，正在尝试登录");
+            console.log(new Date().toLocaleString() + " | [ERR] 网络连接断开，正在尝试登录");
             let uri = "https://portal.ccnu.edu.cn";
             try {
                 await page.goto(uri, {waitUntil: "networkidle2"});
                 await page.click('#login-account');
             } catch (e) {
-                console.log("[ERR] 登录页面加载失败");
+                console.log(new Date().toLocaleString() + " | [ERR] 登录页面加载失败");
                 await page.close();
                 await browser.close();
                 timer = setTimeout(daemon, 10000);
@@ -44,10 +44,11 @@ async function main() {
             }
             try {
                 let ip = await page.$eval("#ipv4", ele => ele.innerHTML);
-                console.log("[INFO] 您已经登陆过了，您的IPv4地址是:" + ip);
+                console.log(new Date().toLocaleString() + " | [INFO] 您已经登录过了，您的IPv4地址是:" + ip);
                 await page.close();
                 await browser.close();
                 timer = setTimeout(daemon, 10000);
+                return;
             } catch (e) {
                 try {
                     await page.goto(uri, {waitUntil: "networkidle2"});
@@ -56,15 +57,17 @@ async function main() {
                     await page.click('#login-account');
                     await page.waitForNavigation({waitUntil: "networkidle2"});
                     let ip = await page.$eval("#ipv4", ele => ele.innerHTML);
-                    console.log("[INFO] 登录成功，您的IPv4地址是:" + ip);
+                    console.log(new Date().toLocaleString() + " | [INFO] 登录成功，您的IPv4地址是:" + ip);
                     await page.close();
                     await browser.close();
-                    setTimeout(daemon, 10000);
+                    timer = setTimeout(daemon, 10000);
+                    return;
                 } catch (e) {
-                    console.log("[ERR] 登录失败");
+                    console.log(new Date().toLocaleString() + " | [ERR] 登录失败");
                     await page.close();
                     await browser.close();
-                    setTimeout(daemon, 10000);
+                    timer = setTimeout(daemon, 10000);
+                    return;
                 }
             }
         }
